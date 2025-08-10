f08f5beb9f87eb2b22c05e0ddb65dc89d3e75c64
zb-gong <gongzib@outlook.com>
2025-08-10 14:48:41 -0700

    Prepare for submsg
    Prepare for construting Struct that represent sub messages.
    
    Signed-off-by: Zibo Gong <gongzib@outlook.com>

--- lib/ynl-priv.h
+++ lib/ynl-priv.h
@@ -27,6 +27,7 @@ enum ynl_policy_type {
   YNL_PT_UINT,
   YNL_PT_NUL_STR,
   YNL_PT_BITFIELD32,
+  YNL_PT_SUBMSG,
 };
 
 enum ynl_parse_result {
@@ -46,6 +47,9 @@ typedef int (
 struct ynl_policy_attr {
   const char* name;
   enum ynl_policy_type type;
+  __u8 is_submsg;
+  __u8 is_selector;
+  __u16 selector_type;
   unsigned int len;
   struct ynl_policy_nest* nest;
 };
@@ -103,6 +107,8 @@ struct nlmsghdr*
 ynl_gemsg_start_dump(struct ynl_sock* ys, __u32 id, __u8 cmd, __u8 version);
 
 int ynl_attr_validate(struct ynl_parse_arg* yarg, const struct nlattr* attr);
+int ynl_submsg_failed(struct ynl_parse_arg *yarg, const char *field_name,
+		      const char *sel_name);
 
 /* YNL specific helpers used by the auto-generated code */
 
